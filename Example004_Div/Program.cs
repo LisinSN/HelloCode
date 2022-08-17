0d1e6120808e399d012a736455866b560e5c998b
Лисин Сергей Николаевич <LisinSN@sklif-b.ru>
2022-08-17 16:37:44 +0300

    Урок 1. Задание 5. Сумма случайных

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,4 @@
-﻿// Урок 1. Задание 4. Частное чисел
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine (numberA / numberB);
+﻿// Урок 1. Задание 5. Сумма случайных
+int numberA =new Random().Next(1,10);
+int numberB = 5;
+Console.WriteLine (numberA + numberB);
