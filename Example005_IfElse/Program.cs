47bc52a1b9ac5916e6248f3d8bad5d74b83f4e4a
Лисин Сергей Николаевич <LisinSN@sklif-b.ru>
2022-08-17 16:52:34 +0300

    Урок 1. Задание 5. Приветствие

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Урок 1. Задача 5. Приветствие 
+Console.Write("Введите имя пользватля: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
